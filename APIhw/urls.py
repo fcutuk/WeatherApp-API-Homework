d4dbc3ced95a728ba2d14ecb8d9efbe656c120c7
fcutuk <cutuk.faris@gmail.com>
2023-05-15 23:44:51 +0200

    update

--- APIhw/urls.py
+++ APIhw/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("weatherApp/", include("weatherApp.urls")),
+    path('weatherApp/', include('weatherApp.urls')),
     path('admin/', admin.site.urls),
 ]
 
