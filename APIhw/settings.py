4b33cc3295eadf3cf329ca3d482fa7806ac826c8
fcutuk <cutuk.faris@gmail.com>
2023-05-16 10:45:55 +0200

    loggin and caching

--- APIhw/settings.py
+++ APIhw/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'request_logging.middleware.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'APIhw.urls'
@@ -122,3 +123,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',  # change debug level as appropiate
+            'propagate': False,
+        },
+    },
+}
--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("/", views.index, name="index"),
     path('weather-current', views.current_weather),
-    path('weather-forecast', views.forcast_weather),
+    path('weather-forcast', views.forcast_weather),
     path('weather-history', views.history_weather),
 ]
