a6a1027dd47df011473df0f903d00d6414167e16
fcutuk <cutuk.faris@gmail.com>
2023-05-20 23:09:16 +0200

    fixed buggs

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -5,126 +5,89 @@ from django.http import JsonResponse
 from django.core.cache import cache
 
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the weather app index.")
 
 def current_weather(request):
+    final_data = {}
     if request.method == 'POST': 
         city = str(request.POST.get('city'))
-        cache_key = 'weather_data_' + city
+        key = '352350089cd0815f99eb9fc26635d7fd'
 
+        cache_key = f'weather_data_{city}'
         cached_data = cache.get(cache_key)
-
         if cached_data:
-            final_data = cached_data
-        else:
-            coordinates = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&units=metric&appid=352350089cd0815f99eb9fc26635d7fd')
-            coordinates_data = coordinates.read().decode('utf-8')
-            list_of_coordinates = json.loads(coordinates_data)
-            latitude = str(list_of_coordinates[0]['lat'])
-            longitude = str(list_of_coordinates[0]['lon'])
+            # If cached data exists, return it directly
+            return JsonResponse(cached_data)
 
-            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?lat='+ latitude + '&lon=' + longitude + '&appid=352350089cd0815f99eb9fc26635d7fd').read()
-            list_of_data = json.loads(source)
+        source = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric")
+        content = source.read().decode('utf-8')
+        final_data = json.loads(content)
 
-            final_data = {
-                'curren_temperature': str(list_of_data['main']['temp']) + ' °C',
-                'feels_like': str(list_of_data['main']['feels_like']) + ' °C',
-                'pressure': str(list_of_data['main']['pressure']),
-                'humidity': str(list_of_data['main']['humidity']),
-                'wind': str(list_of_data['wind']['speed']),
-                'main': str(list_of_data['weather'][0]['main']),
-                'description': str(list_of_data['weather'][0]['description']),
-                'icon': list_of_data['weather'][0]['icon'],
-            }
+    print(final_data)
 
-            cache.set(cache_key, final_data, 300)
+    cache.set(cache_key, final_data, 300)
 
+    if final_data:
+        return HttpResponse("Data retrieved successfully")
     else:
-        final_data = {}
-
-    return render(request, "current_weather_template/index.html", final_data)
+        return HttpResponse("You have entered an invalid location")
 
 
 def forcast_weather(request):
+    final_data = {}
     if request.method == 'POST': 
         city = str(request.POST.get('city'))
-        cache_key = 'weather_data_' + city
+        key = '352350089cd0815f99eb9fc26635d7fd'
 
+        cache_key = f'weather_data_{city}'
         cached_data = cache.get(cache_key)
-
         if cached_data:
-            final_data = cached_data
-        else:
-            coordinates = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&units=metric&appid=352350089cd0815f99eb9fc26635d7fd')
-            coordinates_data = coordinates.read().decode('utf-8')
-            list_of_coordinates = json.loads(coordinates_data)
-            latitude = str(list_of_coordinates[0]['lat'])
-            longitude = str(list_of_coordinates[0]['lon'])
+            # If cached data exists, return it directly
+            return JsonResponse(cached_data)
 
+        source = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=metric")
+        content = source.read().decode('utf-8')
+        final_data = json.loads(content)
 
-            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?lat='+ latitude + 
-                                 '&lon=' + longitude +'&type=hour&appid=ca5a8574962b6e08edf9c49d364192fa').read()
-            list_of_data = json.loads(source)
+    print(final_data)
 
-            final_data={ 
-                'temp_day' : str(list_of_data['temp']['day']),
-                'temp_min' : str(list_of_data['temp']['min']),
-                'temp_max' : str(list_of_data['temp']['max']),
-                'temp_night':  str(list_of_data['temp']['night']),
-                'pressure' : str(list_of_data['pressure']),
-                'humidity' : str(list_of_data['humidity']),
-                'weather_main' : str(list_of_data['weather'][0]['main']),
-                'weather_description' : str(list_of_data['weather'][0]['description']),
-                'weather_icon' : str(list_of_data['weather'][0]['icon']),
-            }
-            print(final_data)
-            cache.set(cache_key, final_data, 300)
+    cache.set(cache_key, final_data, 300)
+
+    if final_data:
+        return HttpResponse("Data retrieved successfully")
     else:
-        final_data = {}
-
-    return render(request, "forcast_weather_template/index.html", final_data)
-
+        return HttpResponse("You have entered an invalid location")
 
 def history_weather(request):
+    final_data = {}
     if request.method == 'POST': 
         city = str(request.POST.get('city'))
-        cache_key = 'weather_data_' + city
+        key = '352350089cd0815f99eb9fc26635d7fd'
 
+        cache_key = f'weather_data_{city}'
         cached_data = cache.get(cache_key)
-
         if cached_data:
-            final_data = cached_data
-        else:
-            coordinates = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&units=metric&appid=352350089cd0815f99eb9fc26635d7fd')
-            coordinates_data = coordinates.read().decode('utf-8')
-            list_of_coordinates = json.loads(coordinates_data)
-            latitude = str(list_of_coordinates[0]['lat'])
-            longitude = str(list_of_coordinates[0]['lon'])
+            # If cached data exists, return it directly
+            return JsonResponse(cached_data)
 
+        coordinates = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&units=metric&appid=352350089cd0815f99eb9fc26635d7fd')
+        coordinates_data = coordinates.read().decode('utf-8')
+        list_of_coordinates = json.loads(coordinates_data)
+        latitude = str(list_of_coordinates[0]['lat'])
+        longitude = str(list_of_coordinates[0]['lon'])
 
-            source = request.urlopen('https://history.openweathermap.org/data/2.5/history/city?lat' + latitude + '&lon=' + 
-                                    longitude + '&type=hour&cnt=' + 10+ '&appid=352350089cd0815f99eb9fc26635d7fd').read()
-            list_of_data = json.loads(source)
+        source = urllib.request.urlopen("https://history.openweathermap.org/data/2.5/history/city?lat=" + latitude + "&lon=" + longitude + "&type=hour&appid=" + key)
+        content = source.read().decode('utf-8')
+        final_data = json.loads(content)
 
-            final_data={ 
-                'dt' : str(list_of_data['dt']),
-                'temp' : str(list_of_data['main']['temp']),
-                'feels_like' : str(list_of_data['main']['feels_like']),
-                'pressure' : str(list_of_data['main']['pressure']),
-                'humidity' : str(list_of_data['main']['humidity']),
-                'temp_min' : str(list_of_data['main']['temp_min']),
-                'temp_max' : str(list_of_data['main']['temp_max']),
-                'wind_speed' : str(list_of_data['wind']['speed']),
-                'wind_deg' : str(list_of_data['wind']['deg']),
-                'clouds' : str(list_of_data['clouds']['all']),
-                'weather_main' : str(list_of_data['weather'][0]['main']),
-                'weather_description' : str(list_of_data['weather'][0]['description']),
-                'weather_icon' : str(list_of_data['weather'][0]['icon']),
-            }
-            print(final_data)
-            cache.set(cache_key, final_data, 300)
+    print(final_data)
+
+    cache.set(cache_key, final_data, 300)
+
+    if final_data:
+        return HttpResponse("Data retrieved successfully")
     else:
-        final_data = {}
-
-    return render(request, "history_weather_template/index.html", final_data)
+        return HttpResponse("You have entered an invalid location")
+    
